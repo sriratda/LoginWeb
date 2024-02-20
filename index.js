b6691810092f397477ffefc9c3264a799382429f
BasTaks <taksin.cup@gmail.com>
2024-02-20 15:52:45 +0700

    create information.ejs

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 const router = require('./src/controller/router');
+const path = require('path');
 
 app.use(express.urlencoded({extended: false}));
 app.use(router);
+
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'ejs');
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- src/controller/router.js
+++ src/controller/router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const fs = require('fs');
 
 router.get('/', (req, res) => {
     res.status(200);
@@ -20,14 +21,65 @@ router.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, '../views/login.html'));
 });
 
-router.get('/information', (req, res) =>{
+router.get('/information', (req, res) => {
+    const filePath = path.join(__dirname, '/data.json');
+
+    function readUsers() {
+        try {
+            const data = fs.readFileSync(filePath, 'utf8');
+            return JSON.parse(data);
+        } catch (err) {
+            console.error('Error reading users file:', err);
+            return [];
+        }
+    }
+
+    const users = readUsers();
+
     res.status(200);
     res.type('text/html');
-    res.sendFile(path.join(__dirname, '../views/information.html'));
+    res.render(path.join(__dirname, '../views/information.ejs'), {data: users});
 });
 
 router.post('/register/user', (req, res) => {
-    console.log(req.body);
+    const data = req.body;
+    const filePath = path.join(__dirname, '/data.json');
+
+    function readUsers() {
+        try {
+            const data = fs.readFileSync(filePath, 'utf8');
+            return JSON.parse(data);
+        } catch (err) {
+            console.error('Error reading users file:', err);
+            return [];
+        }
+    }
+
+    function writeUsers(users) {
+        try {
+            const jsonData = JSON.stringify(users, null, 1);
+            fs.writeFileSync(filePath, jsonData, 'utf8');
+            res.redirect('/information')
+        } catch (err) {
+            console.error('Error writing users file:', err);
+            throw err;
+        }
+    }
+
+    const users = readUsers();
+
+    // Check if the user with the same email already exists
+    const existingUser = users.find(user => user.email === data.email);
+
+    if (existingUser) {
+        console.log('User with the same email already exists.');
+        res.status(400).send('User with the same email already exists.');
+    } else {
+        // Add the new user only if not already present
+        users.push(data);
+        writeUsers(users);
+        res.status(201).send('User registered successfully.');
+    }
 });
 
 module.exports = router;
