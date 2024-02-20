613bc563a94cb8e5cbc3a1186ba040de3e05b5de
BasTaks <taksin.cup@gmail.com>
2024-02-20 17:12:15 +0700

    set path index

--- src/controller/router.js
+++ src/controller/router.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 router.get('/', (req, res) => {
     res.status(200);
     res.type('text/html');
-    res.sendFile(path.join(__dirname, '../views/home.html'));
+    res.sendFile(path.join(__dirname, '../../index.html'));
 });
 
 router.get('/signup', (req, res) => {
